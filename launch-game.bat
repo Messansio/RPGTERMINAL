e3952bdfc0af7bca1fc4dd35a62f17ecc32f4e5d
Messansio <a.messana2@studenti.unipi.it>
2025-02-12 12:12:18 +0100

    added relative paths

--- launch-game.bat
+++ launch-game.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Starting game...
 
-REM Set explicit path
-set GAME_DIR=C:\Users\andre\OneDrive\Documenti\Universita\lab1\RPGTERMINAL
+REM Get current directory
+set GAME_DIR=%~dp0
 
 REM Check if directory exists
 if not exist "%GAME_DIR%" (
@@ -27,7 +27,7 @@ if errorlevel 1 (
 )
 
 REM Launch game with admin rights in Windows Terminal
-powershell Start-Process wt -ArgumentList '-d "%GAME_DIR%" cmd /k "title RPG Terminal && node game.js"' -Verb RunAs
+powershell Start-Process wt -ArgumentList '-d "%~dp0" cmd /k "title RPG Terminal && node game.js"' -Verb RunAs
 
 REM Add pause to see any errors
 if errorlevel 1 (
